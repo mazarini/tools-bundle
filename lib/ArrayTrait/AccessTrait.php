f6bf55ca8457a6fd66a67b50b2ec8bb22dd3d950
Mazarini <mazarini@protonmail.com>
2019-11-18 22:55:48 +0100

    Add array and iterator functions

---
+++ lib/ArrayTrait/AccessTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Copyright (C) 2019 Mazarini <mazarini@protonmail.com>.
+ * This file is part of mazarini/tools-bundle.
+ *
+ * mazarini/tools-bundle is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * mazarini/tools-bundle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ */
+
+namespace Mazarini\ToolsBundle\ArrayTrait;
+
+trait AccessTrait
+{
+    abstract protected function getContainer(): array;
+
+    abstract protected function setContainer(array $container): void;
+
+    public function offsetSet($offset, $value)
+    {
+        $set = 'set'.ucfirst($offset);
+        $this->$set($value);
+    }
+
+    public function offsetExists($offset)
+    {
+        $set = 'set'.ucfirst($offset);
+
+        return method_exists($this, $set);
+    }
+
+    public function offsetUnset($offset)
+    {
+    }
+
+    public function offsetGet($offset)
+    {
+        $get = 'get'.ucfirst($offset);
+        $this->$get();
+    }
+}
